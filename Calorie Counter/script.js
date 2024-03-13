bc9d952397519257004d2d7f2d1404716016a593
SelasiBansa <max.bansa@gmail.com>
2024-03-13 16:06:51 +0000

    small changes

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -32,9 +32,26 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
-  for (const item of list){}
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+  return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
