2ed161de7a520fb4ce24cd2308eb2e83a0c2c147
SelasiBansa <max.bansa@gmail.com>
2024-08-22 19:50:36 +0000

    fcc study

--- Review DOM Manipulation by Building a Rock, Paper, Scissors Game/script.js
+++ Review DOM Manipulation by Building a Rock, Paper, Scissors Game/script.js
@@ -1,23 +1,42 @@
 function getRandomComputerResult() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    return options[randomIndex];
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
   }
-  
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  function hasPlayerWonTheRound(player, computer) {
-    return (
-      (player === "Rock" && computer === "Scissors") ||
-      (player === "Scissors" && computer === "Paper") ||
-      (player === "Paper" && computer === "Rock")
-    );
-  }
-  function getRoundResults(userOption) {
-    const computerResult = getRandomComputerResult();
-   if (player === computerResult)
-  }
-  
-  console.log(getRoundResults("Rock"));
-  console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+}
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption); 
+  playerScoreSpanElement.innerText = playerScore;
+  computerScoreSpanElement.innerText = computerScore; 
+};
+
+showResults("Rock");
